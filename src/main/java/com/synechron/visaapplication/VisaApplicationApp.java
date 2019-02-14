20290cc19645aa57927d6a4d4c00532adc3468f9
Niraj.Bhatt <neerajbhattneeraj@gmail.com>
2019-02-14 14:53:05 +0530

    Adding domain classes and sample data in the repository

--- src/main/java/com/synechron/visaapplication/VisaApplicationApp.java
+++ src/main/java/com/synechron/visaapplication/VisaApplicationApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VisaApplication {
+public class VisaApplicationApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VisaApplication.class, args);
+		SpringApplication.run(VisaApplicationApp.class, args);
 	}
 
 }
---
+++ src/main/java/com/synechron/visaapplication/domain/Validation.java
@@ -0,0 +1,5 @@
+package com.synechron.visaapplication.domain;
+
+public enum Validation {
+  PASSED, FAILED, UNKNOWN
+}
